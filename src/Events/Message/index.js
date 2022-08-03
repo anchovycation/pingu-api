7d68f9dc4db51f7658524f4fe4cdb0fae18c62fa
beyzaerkan <beyzaerkan@icloud.com>
2022-08-03 21:52:54 +0300

    first room saved in message and playlist events

--- src/Events/Message/index.js
+++ src/Events/Message/index.js
@@ -2,8 +2,8 @@ import { SOCKET_EVENTS } from '../../Constants';
 import MessageService from '../../Services/Message'
 
 export const message = async ({id, text, user }, { socket, io }) => {
-  io.to(id).emit(SOCKET_EVENTS.RECEIVE_MESSAGE, {text, user});
   await MessageService.saveMessage(id, text, user);
+  io.to(id).emit(SOCKET_EVENTS.RECEIVE_MESSAGE, {text, user});
 };
 
 export default [
--- src/Events/Playlist/index.js
+++ src/Events/Playlist/index.js
@@ -2,8 +2,8 @@ import { SOCKET_EVENTS } from '../../Constants';
 import RoomService from '../../Services/Room';
 
 export const addVideoToPLaylist = async ({ id, username, link }, { socket, io }) => {
+  await RoomService.addVideoToPLaylist({ id, username, link });
   io.to(id).emit(SOCKET_EVENTS.PLAYLIST_UPDATED, { id, username, link });
-  await RoomService.addVideoToPLaylist({ id, username, link })
 };
 
 export default [
