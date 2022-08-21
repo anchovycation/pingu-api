f2eb58ca818f60d391a6b65b6d53e0c035045b79
beyzaerkan <beyzaerkan@icloud.com>
2022-08-21 13:04:07 +0300

    associated socket events in same file

--- src/Events/Message/index.js
+++ src/Events/Message/index.js
@@ -6,9 +6,17 @@ export const message = async ({id, text, user }, { socket, io }) => {
   io.to(id).emit(SOCKET_EVENTS.RECEIVE_MESSAGE, {text, user});
 };
 
+export const typing = async ({ id, username }, { socket, io }) => {
+  socket.broadcast.to(id).emit(SOCKET_EVENTS.DISPLAY, { username })
+};
+
 export default [
   {
     event: SOCKET_EVENTS.SEND_MESSAGE,
     handler: message,
+  },
+  {
+    event: SOCKET_EVENTS.TYPING,
+    handler: typing,
   },
 ];
--- src/Events/Video/index.js
+++ src/Events/Video/index.js
@@ -18,9 +18,31 @@ export const updateVideoStatus = async (
   io.to(id).emit(SOCKET_EVENTS.VIDEO_STATUS_UPDATED, { video });
 };
 
+export const jumpInVideo = async (
+  { id, duration }, { socket, io }
+) => {
+  let video = await RoomService.jumpInVideo(id, duration);
+  io.to(id).emit(SOCKET_EVENTS.VIDEO_DURATION_CHANGED, { video });
+};
+
+export const skipVideo = async (
+  { id }, { socket, io }
+) => {
+  let room = await RoomService.skipVideo(id);
+  io.to(id).emit(SOCKET_EVENTS.VIDEO_SKIPPED, { video: room.video, playlist: room.playlist });
+};
+
 export default [
   {
     event: SOCKET_EVENTS.UPDATE_VIDEO_STATUS,
     handler: updateVideoStatus,
+  },
+  {
+    event: SOCKET_EVENTS.CHANGE_VIDEO_DURATION,
+    handler: jumpInVideo,
+  },
+  {
+    event: SOCKET_EVENTS.SKIP_VIDEO,
+    handler: skipVideo,
   },
 ];
