568a66f9705c069b9a69f486389c9aa49abc7564
Beyza Erkan <76122007+beyzaerkan@users.noreply.github.com>
2022-08-27 13:09:53 +0300

    video duration is now saved (#58)

---
+++ src/Utilities/Timer.js
@@ -0,0 +1,21 @@
+import RoomService from "../Services/Room";
+
+ class Timer {
+
+  static timers = {};
+
+  static startTimer = async (id) =>  {
+    let room = await RoomService.findRoom(id);
+    this.timers[room.id] = setInterval ( () => {
+      room.video.duration +=  1;
+      room.save();
+    }, 1000);
+  }
+
+  static stopTimer(id) {
+    clearInterval(this.timers[id]);
+    delete this.timers[id];
+  }
+}
+
+export default Timer;
